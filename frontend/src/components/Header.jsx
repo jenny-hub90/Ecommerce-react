a7441348860be4b959dd195b15e508727a58a8b2
jenny-hub90 <jumbledjenny19@gmail.com>
2023-07-13 14:02:41 +0545

    completed add to cart functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,12 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
+import { useSelector } from "react-redux";
 import logo from "../assets/logo.jpg";
+
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   const navbarStyle = {
     // backgroundColor: "#E0EDF4",
     color: "#161616",
@@ -30,6 +34,11 @@ const Header = () => {
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import {
+  Form,
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+} from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import Rating from "../components/Rating";
 import Loader from "../components/loader";
 import Message from "../components/Message";
+import { addToCart } from "../slices/cartSlice";
 
 const ProductScreen = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -18,11 +28,22 @@ const ProductScreen = () => {
   };
 
   const { id: productId } = useParams();
+  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [qty, setQty] = useState(1);
+
   const {
     data: product,
     isLoading,
     error,
   } = useGetProductDetailsQuery(productId);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({...product, qty}));
+    navigate('/cart');
+  }
 
   const productName = {
     fontFamily: "Noto Serif Makasar, serif",
@@ -30,7 +51,6 @@ const ProductScreen = () => {
     textTransform: "Uppercase",
   };
   const productPrice = {
-    // fontFamily: 'Shadows Into Light, cursive',
     fontFamily: "Cardo, serif",
     fontSize: "20px",
   };
@@ -45,7 +65,11 @@ const ProductScreen = () => {
     backgroundColor: "#000",
     color: "#fff",
   };
+  const countInStockArray = product?.countInStock
+    ? [...Array(product.countInStock).keys()]
+    : [];
 
+  console.log(countInStockArray);
   return (
     <>
       <Link
@@ -60,70 +84,99 @@ const ProductScreen = () => {
         Go Back
       </Link>
 
-      { isLoading? (<Loader/>): error? (<Message variant='danger'>{ error?.data?.message || error.error}</Message>):(  <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={4}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3 style={productName}>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item style={productPrice}>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item style={productPrice}>
-              Price: ${product.price}
-            </ListGroup.Item>
-            <ListGroup.Item style={productPrice}>
-              Description: {product.description}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup variant="flush" style={productPrice}>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={4}>
+            <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Price: </Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h3 style={productName}>{product.name}</h3>
               </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Status: </Col>
-                  <Col>
-                    <strong>
-                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
-                    </strong>
-                  </Col>
-                </Row>
+              <ListGroup.Item style={productPrice}>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
               </ListGroup.Item>
-              <ListGroup.Item>
-                <Button
-                  className="btn-block"
-                  type="button"
-                  style={
-                    isHovered
-                      ? { ...buttonStyle, ...buttonHoverStyle }
-                      : buttonStyle
-                  }
-                  onMouseOver={handleMouseOver}
-                  onMouseOut={handleMouseOut}
-                  disabled={product.countInStock === 0}
-                >
-                  Add to Cart
-                </Button>
+              <ListGroup.Item style={productPrice}>
+                Price: ${product.price}
+              </ListGroup.Item>
+              <ListGroup.Item style={productPrice}>
+                Description: {product.description}
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>)}
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush" style={productPrice}>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price: </Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status: </Col>
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty (Number(e.target.value))}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+                <ListGroup.Item>
+                  <Button
+                    className="btn-block"
+                    type="button"
+                    style={
+                      isHovered
+                        ? { ...buttonStyle, ...buttonHoverStyle }
+                        : buttonStyle
+                    }
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+                    disabled={product.countInStock === 0}
+                    onClick={addToCartHandler}
+                  >
+                    Add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
