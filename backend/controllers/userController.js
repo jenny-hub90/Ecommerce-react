85ee259043cb4ad15a5b072a70e0ed36184d9fd2
jenny-hub90 <jumbledjenny19@gmail.com>
2023-07-16 12:10:18 +0545

    added user email and password validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,89 +1,91 @@
 import asyncHandler from "../middleware/asyncHandler.js";
-import User from '../models/userModel.js';
+import User from "../models/userModel.js";
 
 //@desc Auth user & get token
 //@route GET /api/users/login
 //@access Public
-const authUser = asyncHandler(async (req,res) => {
-    const {email, password} = req.body;
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    const user = await User.findOne({email});
+  const user = await User.findOne({ email });
 
-    if(user){
-        res.json({
-            
-        })
-    }
-    res.send('auth user');
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 //@desc Register user
 //@route POST /api/users
 //@access Public
-const registerUser = asyncHandler(async (req,res) => {
-    res.send('register user');
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("register user");
 });
 
 //@desc Logout user/clear cookie
 //@route POST /api/users/logout
 //@access Private
-const logoutUser = asyncHandler(async (req,res) => {
-    res.send('logout user');
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send("logout user");
 });
 
 //@desc Get user profile
 //@route GET /api/users/profile
 //@access Private
-const getUserProfile = asyncHandler(async (req,res) => {
-    res.send('get user profile');
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("get user profile");
 });
 
 //@desc Update user profile
 //@route PUT /api/users/profile
 //@access Private
-const updateUserProfile = asyncHandler(async (req,res) => {
-    res.send('update user profile');
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("update user profile");
 });
-
 
 //@desc Get users
 //@route GET /api/users
 //@access Private/Admin
-const getUsers = asyncHandler(async (req,res) => {
-    res.send('get users');
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get users");
 });
 
 //@desc Get users by ID
 //@route GET /api/users/:id
 //@access Private/Admin
-const getUsersByID= asyncHandler(async (req,res) => {
-    res.send('get users by id');
+const getUsersByID = asyncHandler(async (req, res) => {
+  res.send("get users by id");
 });
 
 //@desc Delete users
 //@route DELETE /api/users/:id
 //@access Private/Admin
-const deleteUsers = asyncHandler(async (req,res) => {
-    res.send('delete user');
+const deleteUsers = asyncHandler(async (req, res) => {
+  res.send("delete user");
 });
 
 //@desc Update users
 //@route PUT /api/users/:id
 //@access Private/Admin
-const updateUser = asyncHandler(async (req,res) => {
-    res.send('update user');
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("update user");
 });
 
-export{
-    authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile,
-    getUsers,
-    deleteUsers,
-    getUsersByID,
-    updateUser
-}
-
-
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUsers,
+  getUsersByID,
+  updateUser,
+};
