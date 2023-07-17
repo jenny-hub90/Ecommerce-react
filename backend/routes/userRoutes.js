940ba01c71a2c7b3eb1658ebd57e100f95d33857
jenny-hub90 <jumbledjenny19@gmail.com>
2023-07-17 14:27:51 +0545

    added JWT coookie, clear cookie, auth middleware, user register, logout, update profile and get profile api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,12 +12,13 @@ import {
     getUsersByID,
     updateUser
 } from '../controllers/userController.js';
+import {protect, admin} from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
-router.post('/profile').get(getUserProfile).put(updateUserProfile);
-router.post('/:id').delete(deleteUsers).get(getUsersByID).put(updateUser);
+router.post('/profile').get(protect, getUserProfile).put(protect,updateUserProfile);
+router.post('/:id').delete(protect, admin, deleteUsers).get(protect, admin, getUsersByID).put(protect, admin, updateUser);
 
 
 export default router;
