61721f827c19df3198d0fa8897d7d2007e9f45e4
jenny-hub90 <jumbledjenny19@gmail.com>
2023-07-18 21:42:14 +0545

    added login functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -17,8 +17,8 @@ import {protect, admin} from '../middleware/authMiddleware.js';
 router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
-router.post('/profile').get(protect, getUserProfile).put(protect,updateUserProfile);
-router.post('/:id').delete(protect, admin, deleteUsers).get(protect, admin, getUsersByID).put(protect, admin, updateUser);
+router.route('/profile').get(protect, getUserProfile).put(protect,updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUsers).get(protect, admin, getUsersByID).put(protect, admin, updateUser);
 
 
 export default router;
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,11 +1,11 @@
-import {USERS_URL} from "../constants";
+import {USER_URL} from "../constants";
 import { apiSlice } from "./apiSlice"; 
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
          query: (data) => ({
-            url: USERS_URL/auth,
+            url: `${USER_URL}/auth`,
             method: 'POST',
             body: data,
          }),
